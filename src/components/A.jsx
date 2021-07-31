bc3d3c781c11046104302a17c11831acc657e302
VictorSan <3292495046@qq.com>
2021-07-31 12:58:56 +0800

    feat: less-loader

--- src/components/A.jsx
+++ src/components/A.jsx
@@ -1,8 +1,14 @@
+import { IonButton } from "@ionic/vue";
 
+import './A.less';
 
-
-const A = props => {
-  return (<div>{props.c}</div>)
-}
+const A = (props) => {
+  return (
+    <div className="component-a">
+      <IonButton>jsx里的按钮组件</IonButton>
+      {props.c}
+    </div>
+  );
+};
 
 export default A;
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  outputDir: 'docs', // https://github.com/victorsonger/victorsonger.github.com/settings/pages  由于github只提供以跟目录或/docs目录来发布项目，所以我们这边选择把打包内容输出到docs目录下
-}
+  outputDir: "docs", // https://github.com/victorsonger/victorsonger.github.com/settings/pages  由于github只提供以跟目录或/docs目录来发布项目，所以我们这边选择把打包内容输出到docs目录下
+  css: {
+    loaderOptions: {
+      // less 8.0之后的版本  配置有修改 https://segmentfault.com/a/1190000039190699
+      // 但是上面链接中的改动 需要配合webpack5才行  本项目是webpack4
+      // 所以采用降低less版本到7.3.0的方案
+      less: {
+      },
+    },
+  },
+};
 // module.exports = {
 //   chainWebpack: (config) => {
 //     config.outputDir = "docs";
